be25a2c95a241ace55ea30bc136e1edef7085f2b
Captain Crom <github@TheAgilePirate.net>
2014-03-17 09:25:37 +0000

    comment out redundant tests (want to migrate these to unit tests eventually

--- IF-Node/IF-Node/js/server/tests.js
+++ IF-Node/IF-Node/js/server/tests.js
@@ -25,7 +25,7 @@ exports.Tests = function Tests() {
         console.log('====Game Object Creation tests====');
         var a0 = new artefact.Artefact('artefact', 'an artefact of little consequence', 'not much to say really',1,'junk', true, false, false, false, null);
         //(aname, aDescription, aDetailedDescription, weight, aType, carryWeight, health, affinity, carrying)
-        var c0 = new creature.Creature('creature','a beastie', 'a big beastie with teeth',120, 'creature', 50, 150, 0);
+        var c0 = new creature.Creature('creature','a beastie', 'a big beastie with teeth',120, 'unknown','creature', 50, 150, 0);
         var e0 = new exit.Exit('north','test0'); //note we can name a location that doesn't exist at the moment - should probably prevent this.
         var l0 = new location.Location('test0','a test location');
         var m0 = new map.Map();
@@ -34,15 +34,15 @@ exports.Tests = function Tests() {
         console.log('====Action tests====');
         var act0 = new action.Action('test action', p0, m0, null); //last param is a dictionary
         //test string splitting on 'with', 'to', 'from', 'for', 'at', 'on', 'in'
-        console.log('Split results: '+ act0.testStringSplit('eat some food with a fork on a stick'));  //test with
-        console.log('Split results: '+ act0.testStringSplit('give some tofu to the vegan within the well')); //test to
-        console.log('Split results: '+ act0.testStringSplit('take the fromage frais from the man from del monte')); //test from
-        console.log('Split results: '+ act0.testStringSplit('draw a forest for the forest shrew')); //test for
-        console.log('Split results: '+ act0.testStringSplit('throw the cat hairball at the patch of mud')); //test at
-        console.log('Split results: '+ act0.testStringSplit('put one penny on the money counter')); //test on
-        console.log('Split results: '+ act0.testStringSplit('put 50 indian rupees in the savings jar')); //test in
-        console.log('Split results: '+ act0.testStringSplit('eat a slice of chocolate orange')); //test no split
-        console.log('Split results: '+ act0.testStringSplit('')); //test empty string
+        //console.log('Split results: '+ act0.testStringSplit('eat some food with a fork on a stick'));  //test with
+        //console.log('Split results: '+ act0.testStringSplit('give some tofu to the vegan within the well')); //test to
+        //console.log('Split results: '+ act0.testStringSplit('take the fromage frais from the man from del monte')); //test from
+        //console.log('Split results: '+ act0.testStringSplit('draw a forest for the forest shrew')); //test for
+        //console.log('Split results: '+ act0.testStringSplit('throw the cat hairball at the patch of mud')); //test at
+        //console.log('Split results: '+ act0.testStringSplit('put one penny on the money counter')); //test on
+        //console.log('Split results: '+ act0.testStringSplit('put 50 indian rupees in the savings jar')); //test in
+        //console.log('Split results: '+ act0.testStringSplit('eat a slice of chocolate orange')); //test no split
+        //console.log('Split results: '+ act0.testStringSplit('')); //test empty string
 
         console.log('====End of Action tests====');
 
@@ -63,14 +63,14 @@ exports.Tests = function Tests() {
 
 
         //test maps
-        m0.init(p0);
-        m0.addLocation('test2','yet another test location');
-        console.log(m0.findLocation('test2'));
-        console.log(m0.link('u','start','test2'));
+        //m0.init(p0);
+        //m0.addLocation('test2','yet another test location');
+        //console.log(m0.findLocation('test2'));
+        //console.log(m0.link('u','start','test2'));
         //console.log(p0.go(null,l0));
-        var l1 = new location.Location('test1','another test location');
-        l0.addExit('n',l1);
-        console.log(l0.getExit());
+        //var l1 = new location.Location('test1','another test location');
+        //l0.addExit('n',l1);
+        //console.log(l0.getExit());
         //l1.addExit('s',l0); //this seems to overwrite the previous
         //console.log(p0.go(null,l0));
         //
