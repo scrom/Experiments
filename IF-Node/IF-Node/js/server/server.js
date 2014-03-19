84a1725250c6133c5ab891f4f3cebf644a4c7fc9
Captain Crom <github@TheAgilePirate.net>
2014-03-19 16:52:33 +0000

    migrate server to decent coding std. Note mixed use of _ for member variables and not using _ for module dependencies

--- IF-Node/IF-Node/js/server/server.js
+++ IF-Node/IF-Node/js/server/server.js
@@ -1,85 +1,63 @@
 "use strict";
 exports.Server = function Server(anInterpreter) {
     try{
-        var self = this; //closure so we don't lose thisUi refernce in callbacks
-        var objectName = 'Server'; //for reference
-        self.interpreter = anInterpreter;
+        var self = this; //closure so we don't lose this reference in callbacks
+        var _objectName = 'Server'; //for reference
+        var _interpreter = anInterpreter;
 
         //module deps
-        var root = __dirname+'/';
+        var _root = __dirname+'/';
         var express = require('express');
-        var path = require('path');
-        var configObjectModule = require('./config');
+        var _webServer = express();
 
-        self.webServer = express();
+        var configObjectModule = require('./config');
+        var _config = new configObjectModule.Config();
+
         //Array of responses awaiting replies
-        self.waitingResponses=[];
+        var _waitingResponses=[];
 
         //log requests
-        self.webServer.use(express.logger('dev'));
+        _webServer.use(express.logger('dev'));
 
-        self.config = new configObjectModule.Config();
 
-        //Function that will send a message to each waiting response
-        self.sendToWaitingResponses = function() {
- 
-            //If there are some waiting responses
-            if (self.waitingResponses.length) {
-     
-                //For each one - respond with 'Hello World - <current timestamp>'
-                for (var i = 0; i < self.waitingResponses.length; i++) {
-                    var res = self.waitingResponses.pop();
-                    //res.write('id: ' + messageCount + '\n');
-                    res.writeHead(200, {//'Content-type':'text/plain'
-                        'Content-Type': 'text/event-stream',
-                        'Cache-Control': 'no-cache',
-                        'Connection': 'keep-alive'
-                    });
-                    res.write('\n');
-                    res.write("data: " + 'message fired '+ (new Date().getTime()) + '\n\n'); // Note the extra newline
-                    res.end();
-                }
-            }
-        }
-
-        self.webServer.configure(function () {
+        _webServer.configure(function () {
             //serve static files from project root
-            self.webServer.use(express.static(__dirname + '/../../'));
+            _webServer.use(express.static(_root + '../../'));
 
-            self.webServer.get('/config', function (request, response) {
-                response.send(self.interpreter.translate(request.url,self.config));
+            _webServer.get('/config', function (request, response) {
+                response.send(_interpreter.translate(request.url,_config));
             });
 
-           self.webServer.get('/new/*', function (request, response) {
-                response.send(self.interpreter.translate(request.url,self.config));
+           _webServer.get('/new/*', function (request, response) {
+                response.send(_interpreter.translate(request.url,_config));
             });
 
-           self.webServer.get('/list*', function (request, response) {
-                response.send(self.interpreter.translate(request.url,self.config));
+           _webServer.get('/list*', function (request, response) {
+                response.send(_interpreter.translate(request.url,_config));
             });
 
-            self.webServer.get('/action/*', function (request, response) {
-                response.send(self.interpreter.translate(request.url,self.config));
+            _webServer.get('/action/*', function (request, response) {
+                response.send(_interpreter.translate(request.url,_config));
             });
 
             //event source handler(!ooh) 
-            self.webServer.get('/events*', function (request, response) {
+            _webServer.get('/events*', function (request, response) {
                 request.socket.setTimeout(0);
 
                 //var messagecount = 0;
                 
                 //Add the response object to the array of waiting responses
                 //To be replied to at some point by the sendToWaitingResponses() method
-                self.waitingResponses.push(response); 
+                _waitingResponses.push(response); 
 
-                //response.send(interpreter.translate(request.url,config));
+                //response.send(_interpreter.translate(request.url,_config));
 
             });
 
             //fire an event
-            self.webServer.get('/fire*', function (request, response) {
+            _webServer.get('/fire*', function (request, response) {
                 response.writeHead(200, {'Content-type':'text/plain'});
-                response.write('firing '+self.waitingResponses.length+' messages...');  
+                response.write('firing '+_waitingResponses.length+' messages...');  
                               
                 self.sendToWaitingResponses();
                 //#'hack = this never seems to work on the first fire
@@ -90,22 +68,46 @@ exports.Server = function Server(anInterpreter) {
             });
 
             //serve default dynamic
-            self.webServer.get('*', function (request, response) {
-                response.send(self.interpreter.translate(request.url,self.config));
+            _webServer.get('*', function (request, response) {
+                response.send(_interpreter.translate(request.url,_config));
             });
         });
 
-        console.log(objectName + ' created');
+        //public member functions
+
+        //Function that will send a message to each waiting response - used for eventsourcing
+        self.sendToWaitingResponses = function() {
+ 
+            //If there are some waiting responses
+            if (_waitingResponses.length) {
+     
+                //For each one - respond with 'Hello World - <current timestamp>'
+                for (var i = 0; i < _waitingResponses.length; i++) {
+                    var res = _waitingResponses.pop();
+                    //res.write('id: ' + messageCount + '\n');
+                    res.writeHead(200, {//'Content-type':'text/plain'
+                        'Content-Type': 'text/event-stream',
+                        'Cache-Control': 'no-cache',
+                        'Connection': 'keep-alive'
+                    });
+                    res.write('\n');
+                    res.write("data: " + 'message fired '+ (new Date().getTime()) + '\n\n'); // Note the extra newline
+                    res.end();
+                };
+            };
+        };
+
+       //initiate listening with port from config
+       self.listen = function () {
+            self = this;
+            _webServer.listen(_config.port)
+            console.log(_objectName + ' '+_config.hostname+' listening on port ' + _config.port);
+        };
+
+        console.log(_objectName + ' created');
     }
     catch (err) {
         console.log('Unable to create Server object: ' + err);
-    }
-
-    //initiate listening with port from config
-    Server.prototype.listen = function () {
-        self = this;
-        self.webServer.listen(self.config.port)
-        console.log(objectName + ' '+self.config.hostname+' listening on port ' + self.config.port);
     };
-return this;
-}
+
+};
