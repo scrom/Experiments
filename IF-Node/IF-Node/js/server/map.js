9cb4ffa00d4a068c32f80d095684f793b9d0cd02
Captain Crom <github@TheAgilePirate.net>
2014-03-25 07:31:55 +0000

    Expand map to most of ground floor

--- IF-Node/IF-Node/js/server/map.js
+++ IF-Node/IF-Node/js/server/map.js
@@ -64,19 +64,78 @@ exports.Map = function Map() { //inputs for constructor TBC
 
             var reception = self.addLocation('reception',"You are stood by the big red reception desk in the Red Gate office atrium.");
             var toilet = self.addLocation('toilet-ground-floor',"You stare at yourself in the mirror of that bathroom and muse over the form and design of the soap dispensers.<br>It's probably not socially acceptable to hang around in here all day though.");
-            var lift = self.addLocation('lift-ground-floor',"The lift doors close behind you. You're in the ground floor lift. It's quite dark in here and every now and again a disembodied voice chants something about electrical faults.<br>You contemplate pressing the alarm button but it'll only route to a call centre somewhere.");
+            var lift = self.addLocation('lift-ground-floor',"The lift doors automatically close behind you. You're in the ground floor lift. It's quite dark in here and every now and again a disembodied voice chants something about electrical faults.<br>You contemplate pressing the alarm button but it'll only route to a call centre somewhere.");
+            var bottomStairs = self.addLocation('stairs-ground-floor',"You're standing at the foot of the stairs.");
+            var library = self.addLocation('library',"You're in the atrium Library."); //add comfy sofa and shelves containing books
+            var bottomkitchen = self.addLocation('kitchen-ground-floor',"You're in the atrium kitchen."); //add comfy sofa and shelves containing books
+            var seatingArea = self.addLocation('atrium-seating',"You're in the atrium seating area."); //add chairs, chess, trees
+            //need to add door to mewburn ellis lawyers
+           // var lawyerEntrance = self.addLocation('lawyer entrance',"You're in the entranece to the resident law firm."); //add chairs, chess, trees
+            var groundNorthWestCorridor = self.addLocation('northwest-corridor-ground-floor',"You're in the North-West corridor of the ground floor.");
+            var groundShower = self.addLocation('shower-ground-floor',"You're in the ground floor showers. There's a lingering smell of deodorant, damp and cyclist sweat.");
+            var pioneer = self.addLocation('pioneer',"You're in the Pioneer meeting room. The acoustics in here seem very strange."); //add shiny table
+            var groundWestCorridor = self.addLocation('west-corridor-ground-floor',"You're in the West corridor of the ground floor.");//add water cooler
+            var opportunitiesSouth = self.addLocation('opportunities-south',"You're in south end of the opportunities and ventures area.");//add horn-rimmed glasses
+            var opportunitiesNorth = self.addLocation('opportunities-north',"You're in north end of the opportunities and ventures area.");//add whiteboards
+            var groundBackStairsWest = self.addLocation('back-stairs-ground-floor-west',"You're in the bottom of the west fire escape stairs.");
+            //need to add corridors to peacock and ???
+            var groundSouthWestCorridor = self.addLocation('southwest-corridor-ground-floor',"You're in the South-West corridor of the ground floor.");
+            var groundWestEndSouthCorridor = self.addLocation('west-end-south-corridor-ground-floor',"You're in the West end of the South corridor on the ground floor.");
+            var groundEastEndSouthCorridor = self.addLocation('east-end-south-corridor-ground-floor',"You're in the East end of the South corridor on the ground floor.");
+            var groundSouthEastCorridor = self.addLocation('southeast-corridor-ground-floor',"You're in the South-East corridor of the ground floor.");
+            var poppy = self.addLocation('poppy',"You're in the Poppy meeting room. The AV equipment in here could use some love."); //add AV kit
+            var graffitib = self.addLocation('graffiti-b',"You're in the Graffiti-B meeting room. It's all bright and shiny."); //add room divider
+            var graffitia = self.addLocation('graffiti-a',"You're in the Graffiti-A meeting room. It's all bright and shiny."); //add room divider and good AV kit
+            var room404 = self.addLocation('room-404',"You're in Room 404. It's like a giant 'notfound' error staring at you from beneath a bowler hat.");
+            var groundEastSouthEndCorridor = self.addLocation('east-south-end-corridor-ground-floor',"You're in the South end of the East corridor on the ground floor.");
+            //need to add fire exit to smoking area and fire escape
+            var groundEastCorridor = self.addLocation('east-corridor-ground-floor',"You're in the East corridor of the ground floor.");
+            var restArea = self.addLocation('rest-area-ground-floor',"You're in the ground floor rest area.");//add sofa, hammock, vending machine
+            var groundNorthEastCorridor = self.addLocation('northeast-corridor-ground-floor',"You're in the North end of the ground floor East corridor.");
+            var signpost = self.addLocation('signpost',"You're in the SignPost meeting room.");
+            var buenosAires = self.addLocation('buenosaires',"You're in the Buenos Aires interview room."); //add computer and screen
 
             self.link('e', _locations[atrium].getName(), _locations[reception].getName());
             self.link('s', _locations[atrium].getName(), _locations[toilet].getName());
             self.link('i', _locations[atrium].getName(), _locations[lift].getName());
+            self.link('w', _locations[atrium].getName(), _locations[bottomStairs].getName());
+            self.link('e', _locations[reception].getName(), _locations[seatingArea].getName());
+            self.link('s', _locations[reception].getName(), _locations[groundNorthEastCorridor].getName());
+            self.link('n', _locations[bottomStairs].getName(), _locations[library].getName());
+            self.link('w', _locations[bottomStairs].getName(), _locations[bottomkitchen].getName());
+            self.link('s', _locations[bottomStairs].getName(), _locations[groundNorthWestCorridor].getName());
+            self.link('w', _locations[groundNorthWestCorridor].getName(), _locations[pioneer].getName());
+            self.link('e', _locations[groundNorthWestCorridor].getName(), _locations[groundShower].getName());
+            self.link('s', _locations[groundNorthWestCorridor].getName(), _locations[groundWestCorridor].getName());
+            self.link('s', _locations[groundWestCorridor].getName(), _locations[groundSouthWestCorridor].getName());
+            self.link('w', _locations[groundWestCorridor].getName(), _locations[opportunitiesSouth].getName());
+            self.link('n', _locations[opportunitiesSouth].getName(), _locations[opportunitiesNorth].getName());
+            self.link('n', _locations[opportunitiesNorth].getName(), _locations[groundBackStairsWest].getName());
+            self.link('w', _locations[groundSouthWestCorridor].getName(), _locations[poppy].getName());
+            self.link('e', _locations[groundSouthWestCorridor].getName(), _locations[groundWestEndSouthCorridor].getName());
+            self.link('e', _locations[groundWestEndSouthCorridor].getName(), _locations[groundEastEndSouthCorridor].getName());
+            self.link('n', _locations[groundWestEndSouthCorridor].getName(), _locations[graffitib].getName());
+            self.link('n', _locations[groundEastEndSouthCorridor].getName(), _locations[graffitia].getName());
+            self.link('e', _locations[groundEastEndSouthCorridor].getName(), _locations[groundSouthEastCorridor].getName());
+            self.link('e', _locations[groundSouthEastCorridor].getName(), _locations[room404].getName());
+            self.link('n', _locations[groundSouthEastCorridor].getName(), _locations[groundEastSouthEndCorridor].getName());
+            self.link('n', _locations[groundEastSouthEndCorridor].getName(), _locations[groundEastCorridor].getName());
+            self.link('e', _locations[groundEastSouthEndCorridor].getName(), _locations[restArea].getName());
+            self.link('n', _locations[groundEastCorridor].getName(), _locations[groundNorthEastCorridor].getName());
+            self.link('e', _locations[groundEastCorridor].getName(), _locations[signpost].getName());
+            self.link('e', _locations[groundNorthEastCorridor].getName(), _locations[buenosAires].getName());
+
+            
+
+
 
             var liftEntrance = _locations[atrium].getExit('i');
             liftEntrance.hide();
 
             _locations[atrium].addObject(new artefactObjectModule.Artefact('button', 'a lift call button', "If you push the button, perhaps a lift will come.", 250, 0,'door', false, false, true, false, false, liftEntrance));
             _locations[atrium].addObject(new artefactObjectModule.Artefact('sword', 'an ornamental sword', "It's flimsy and fake-looking but kind of fun.", 3, 25, 'weapon', true, false, false, false, false, null));
-            _locations[atrium].addObject(new artefactObjectModule.Artefact('brick', 'a brick', "This would make quite a good cudgel.", 2, 15, 'weapon', true, false, false, false, false, null));
-            _locations[atrium].addObject(new artefactObjectModule.Artefact('coffee', 'a cup of coffee', "Well, you could either drink this one or give it to someone else.", 1, 3, 'food', true, false, false, true, false, null));        
+            _locations[room404].addObject(new artefactObjectModule.Artefact('brick', 'a brick', "This would make quite a good cudgel.", 2, 15, 'weapon', true, false, false, false, false, null));
+            _locations[bottomkitchen].addObject(new artefactObjectModule.Artefact('coffee', 'a cup of coffee', "Well, you could either drink this one or give it to someone else.", 1, 3, 'food', true, false, false, true, false, null));        
             var liftExit = _locations[lift].getExit('o');
             liftExit.hide();
 
