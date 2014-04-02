4750d263e55e2dc6bdeb92fb707b9e4c67531452
Captain Crom <github@TheAgilePirate.net>
2014-04-02 07:55:04 +0100

    dummy object stub - used as object template for coding. Not part of game code itself

---
+++ IF-Node/IF-Node/js/server/objectStub.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+//object stub - copy this for new objects
+module.exports.ObjectStub = function ObjectStub() { //inputs for constructor TBC
+    try{      
+	    var self = this; //closure so we don't lose this reference in callbacks
+
+	    var _objectName = "ObjectStub";
+        console.log(_objectName + ' created: '+_name+', '+_destinationName);
+
+        ////public methods
+        self.toString = function() {
+            return _objectName;
+        };
+
+
+        ////end public methods
+    }
+    catch(err) {
+	    console.log('Unable to create ObjectStub object: '+err);
+    };	
+};
