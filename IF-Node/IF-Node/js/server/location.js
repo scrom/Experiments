33c1a595effc3e96178e57de3de819a463f00f1d
Captain Crom <github@TheAgilePirate.net>
2014-04-09 18:08:45 +0100

    Migrate location "_objects" to use _inventory and inventory object - brings consistency and reduces duplication

--- IF-Node/IF-Node/js/server/location.js
+++ IF-Node/IF-Node/js/server/location.js
@@ -13,7 +13,7 @@ exports.Location = function Location(aName, aDescription, isDark) {
         var _visits = 0;
         var _dark = isDark;
         var _description = aDescription;
-        var _objects = []; //and creatures
+        var _inventory =  new inventoryObjectModule.Inventory(99999);//unlimited //[]; //and creatures
         var _exits = [];
 
 	    var objectName = "Location";
@@ -75,75 +75,29 @@ exports.Location = function Location(aName, aDescription, isDark) {
         };
 
         self.addObject = function(anObject) {
-            _objects.push(anObject);
-            console.log(anObject+' added to location');
-            return anObject.getDisplayName()+' added to location';
+            return "location is "+_inventory.add(anObject);
         };
 
         self.removeObject = function(anObject) {
             console.log('removing '+anObject+' from '+self.getName());
-
-            for(var index = 0; index < _objects.length; index++) {
-                if(_objects[index].getName() == anObject) {
-                    console.log('creature/object found at location: '+anObject+' index: '+index);
-                    var returnObject = _objects[index];
-                    if (returnObject.isCollectable()||returnObject.canTravel()) {
-                        _objects.splice(index,1);
-                        console.log(anObject+' removed from location');
-                        if (returnObject.isCollectable()) {
-                            return returnObject;//+' removed from location';
-                        };
-                    } else {console.log(anObject+" is not collectable or won't follow");}
-                } else if(_objects[index].getType() == "container") {
-                    //hack - remove item from container and return to caller.
-                    var tempInventory = new inventoryObjectModule.Inventory;
-                    _objects[index].relinquish(anObject, tempInventory); //throw the response away.
-                    var returnedContainerObjects = tempInventory.getAllObjects();
-                    if (returnedContainerObjects.length>0) {return returnedContainerObjects[0];};
-                            
-                };
-            };
+            return _inventory.remove(anObject);
         };
 
         self.objectExists = function(anObjectName) {
-            self = this;
             //check if passed in object is in location
-            //we don't have name exposed any more...
-            if (self.getObject(anObjectName)) {return true;};
-            return false;
+            return _inventory.check(anObjectName);
         };
 
         self.getObject = function(anObjectName) {
-            for(var index = 0; index < _objects.length; index++) {
-                if(_objects[index].getName() == anObjectName) {
-                    console.log('found: '+anObjectName);
-                    return _objects[index];
-                };
-                if(_objects[index].getType() == 'container' && (!(_objects[index].isLocked()))) {
-                    console.log('found container, searching for: '+anObjectName);
-                    if (_objects[index].isOpen()) {
-                    //only confirm item from open, unlocked containers - this way we know the player has discovered them
-                        var object = _objects[index].getInventoryObject().getObject(anObjectName);
-                        if (object) {return object}; 
-                    };
-                };
-           };
-           return null;
+            return _inventory.getObject(anObjectName);
         };
 
         self.getObjectByType = function(anObjectType) {
-            for(var i = 0; i < _objects.length; i++) {
-                if(_objects[i].getType() == anObjectType) {
-                    console.log('found: '+anObjectType);
-                    return _objects[i];
-                };
-           };
-           return null;
+            return _inventory.getObjectByType(anObjectType);
         };
 
         self.getAllObjects = function() {
-            self = this;
-            return _objects;
+            return _inventory.getAllObjects();
         };
 
         self.getDescription = function() {
@@ -152,7 +106,7 @@ exports.Location = function Location(aName, aDescription, isDark) {
 
         self.describe = function() {
             var fullDescription = _description;
-            if (_objects.length > 0) {
+            if (_inventory.size() > 0) {
                 //clean up grammar here (there is/there are)
                 fullDescription+="<br>You can see "+self.listObjects()+".";
             };
@@ -205,39 +159,16 @@ exports.Location = function Location(aName, aDescription, isDark) {
         };
 
         self.listObjects = function() {
-            var list = ''
-            for(var i = 0; i < _objects.length; i++) {
-                    if ((i>0)&&(i<_objects.length-1)){list+=', ';};
-                    if ((i==_objects.length-1)&&(i>0)){list+=' and ';};
-                    list+=_objects[i].getDescription();
-            };
-            return list;
+            return _inventory.listObjects();
         };
 
         self.creaturesExist = function() {
-            for(var i = 0; i < _objects.length; i++) {
-                if(_objects[i].getType() == 'creature') {
-                    console.log('Location contains at least one creature: '+_objects[i].getName());
-                        return true;
-                };
-            };
+            if (self.getCreatures().length > 0) {return true;};
             return false;
         };
 
         self.getCreatures = function() {
-            var creatures = []
-            for(var i = 0; i < _objects.length; i++) {
-                if(_objects[i].getType() == 'creature') {
-                         creatures.push(_objects[i]);;
-                };
-            };
-            return creatures;
-        };
-
-        self.identifyThing = function(anObjectOrCreature) {
-            var anObject = self.getObject(anObjectOrCreature);
-            if (anObject != undefined) {return aCreature.getType();};
-            return null;
+            return _inventory.getAllObjectsOfType('creature');
         };
 
         //end public member functions
