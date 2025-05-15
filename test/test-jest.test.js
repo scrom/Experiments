79704d858baae7007f25c26126c63ed66ac82805
Captain Crom <409676+scrom@users.noreply.github.com>
2025-05-15 15:27:23 +0100

    issue #504 adding Jest test support
    With these changes I can successfully discover and run tests calling Jest from the VS powershell CLI. *however* the built in VS test runner is throwing an error during discovery.
    
    From googling, it appears there is a new bug in the current version of VS that's causing this.
    
    https://developercommunity.visualstudio.com/t/Loading-tests-caused-exception-in-Javas/10902033?q=jest
    
    Starting Test Discovery
    Method not found: ‘System.ReadOnlySpan’1<Char> Microsoft.IO.Path.GetFileName(System.ReadOnlySpan’1<Char>)’.
    An exception occurred while loading the tests by the test discoverer ‘JavascriptProjectTestDiscoverer’. Exception: Method not found: ‘System.ReadOnlySpan’1<Char> Microsoft.IO.Path.GetFileName(System.ReadOnlySpan’1<Char>)’.
    No tests are available in D:\Repositories\tmp\NodeConsoleApp1\NodeConsoleApp1.esproj. Verify that the test discoverers and runners are registered. Also, verify that the platform and framework version settings are appropriate, and then try again.
    
    Checking changes in anyway before reverting to Mocha (or something else)

---
+++ test/test-jest.test.js
@@ -0,0 +1,11 @@
+// Jest test file
+
+const { describe, it, expect } = require('@jest/globals');
+
+describe('test-jest', () => {
+  it('should return the expected result', () => {
+    const input = 'sample input';
+    const expectedOutput = 'expected output'; // Replace with actual expected output
+      expect(input).toBe(expectedOutput);
+  });
+});
