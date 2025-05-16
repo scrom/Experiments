eb36f5e470d19bec2f26498b298276591f1a1f00
Captain Crom <409676+scrom@users.noreply.github.com>
2025-05-16 15:13:08 +0100

    Revert "restructure server and test folders to match client/language convention"
    This reverts commit 23cf6a5641f3aeba75e48a4e812be4dc894b99b3.

---
+++ client/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+//main bootstrap code for game client
+var serverHost = "mvta.herokuapp.com";
+var serverPort;
+var ui;
+var client;
+var debug = false; //enable/disable client console logging
+	
+function init(window, body, statusBar, specialReport, state, input, inputArea, interaction, events, console, image) {
+    //create UI
+	ui = new Ui(body, statusBar, specialReport, state, input, inputArea, interaction, events, console, image);
+
+    //set initial width
+    ui.setWidth();
+    
+    //set initial message:
+    ui.setState('Welcome to MVTA.<br>Your "Minimum Viable Text Adventure" experience starts here...<br><br>Please type in your name and press &lt;enter&gt; <i>(on your keyboard)</i> to start.');
+
+    //handle resizing
+	var resizeTimer;
+	window.resize(function () {
+	    clearTimeout(resizeTimer);
+	    resizeTimer = setTimeout(ui.setWidth, 250);
+	});
+
+    //create client
+    client = new Client(serverHost, serverPort, ui);
+    //client.readServerConfig();
+
+    //set focus on input
+    input.focus();
+
+    //start listening
+    client.listenForInput();
+    client.listenForEvents();
+};
