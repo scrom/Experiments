bfa4eed24a73b08f63e8ca11fef3009aa1cabf1d
Captain Crom <409676+scrom@users.noreply.github.com>
2025-05-31 14:16:23 +0100

    #521 and others - add V2 UI base
    keeping current UI unchanged and working, add support for newer UI versions.
    
    New UI will use a different index, main css and additional libraries but will not touch existing work.
    
    Only code change desired in current UI implementation is to deterine whether to use legacy resizing fuctions  (new UI  will be more responsive - hopefully)

--- client/js/main.js
+++ client/js/main.js
@@ -7,22 +7,28 @@ var ui;
 var client;
 var debug = false; //enable/disable client console logging
 	
-function init(window, body, statusBar, specialReport, state, input, inputArea, interaction, events, console, image) {
+function init(window, body, statusBar, specialReport, state, input, inputArea, interaction, events, console, image, version) {
+    // set ui version    
+    const uiversion = version || 0;
+
+    
     //create UI
 	ui = new Ui(body, statusBar, specialReport, state, input, inputArea, interaction, events, console, image);
-
-    //set initial width
-    ui.setWidth();
     
     //set initial message:
     ui.setState('Welcome to MVTA.<br>Your "Minimum Viable Text Adventure" experience starts here...<br><br>Please type in your name and press &lt;enter&gt; <i>(on your keyboard)</i> to start.');
 
-    //handle resizing
-	var resizeTimer;
-	window.resize(function () {
-	    clearTimeout(resizeTimer);
-	    resizeTimer = setTimeout(ui.setWidth, 250);
-	});
+    if (uiversion < 2) {
+        //set initial width
+        ui.setWidth();
+
+        //handle resizing
+        var resizeTimer;
+        window.resize(function () {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(ui.setWidth, 250);
+        });
+    };
 
     //create client
     client = new Client(serverHost, serverPort, ui);
