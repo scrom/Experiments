64e4061310255e6e3d398b86d59e25b2f3b1a638
Captain Crom <409676+scrom@users.noreply.github.com>
2025-06-02 18:47:28 +0100

    #520 some character name/image cleanup
    find characters whose image files were missing and fix them up

--- build/js/automation/data/listAllCreatureNames.js
+++ build/js/automation/data/listAllCreatureNames.js
@@ -18,7 +18,12 @@ fs.readdirSync(dataDir).forEach(file => {
       //const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
       const json = require(filePath);
       if (json.object === 'creature') {
-        console.info("File: "+file+" ::name: "+json.name+", ::displayName: "+json.displayName+", ::imageName: "+json.attributes.imageName+", ::description: "+json.description);
+        let imageExists = false;
+        if (json.attributes.imageName) {
+          let imagePath = path.join(dataDir+"/images/", json.attributes.imageName);
+          if (fs.existsSync(imagePath)) {imageExists = true};
+        };
+        console.info("File: "+file+" ::name: "+json.name+", ::displayName: "+json.displayName+", ::imageName: "+json.attributes.imageName+"("+imageExists+"), ::description: "+json.description);
         attributeForenameSet.add(json.name.split(" ")[0]);
         attributeSurnameSet.add(json.name.slice(json.name.indexOf(' ') + 1));
       }
