cc7455847080a6764db2f0233fbd3e565cd78e2d
Captain Crom <409676+scrom@users.noreply.github.com>
2025-06-06 15:28:48 +0100

    configure code coverage for Jest
    finally - we can get meaningful coverage and see where things are being missed.
    
    Coverage so far is relatively good for a legacy codebase - but plenty of gaps!
    
    Down the line, I will break builds/block deploys if coverage falls.

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ const config = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "/coverage",
+  coverageDirectory: "<rootDir>/test/jest/coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -122,7 +122,10 @@ const config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "test/jest/",
+  roots: [
+    "<rootDir>/server",
+    "<rootDir>/test/jest"
+  ],
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
